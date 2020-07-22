8fcf79071bd565a64e7f9da766f13c97d9424b2f
ivanborodkin97 <boroda0360@mail.ru>
2020-07-22 22:42:03 +0300

    add file

---
+++ GalToLit.java
@@ -0,0 +1,9 @@
+class GalToLit {
+	public static void main(String[] args) {
+		double gallons; // объем жидкости в голоннах
+		double liters; // объем жидкости в литрах
+		gallons = 10; // начаьное значение
+		liters = gallons * 3.7854; // переводим в литры
+		System.out.println(gallons + " галлонам соответсвует " + liters + " литров!");
+	}
+}
